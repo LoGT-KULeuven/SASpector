d25ea76cfb386146cfc5ddc4b77a0fe7e2dd83bf
emmaver <50531662+emmaver@users.noreply.github.com>
2019-11-24 16:02:24 +0100

    add coverage part

--- tool/SASpector.py
+++ tool/SASpector.py
@@ -11,6 +11,7 @@ from summary import extract_main
 from gene_predict import prokka, blast
 from kmer import kmer
 from quastunmap import quast
+from coverage import cvg_main
 
 import argparse
 import os
@@ -18,14 +19,15 @@ import os
 
 
 def main():
-    parser = argparse.ArgumentParser(prog = 'SASpector - Short-read Assembly inSpector', description = '')
-    parser.add_argument('reference', type = str, help = 'Hybrid assembly FASTA file as reference genome')
-    parser.add_argument('contigs', type = str, help = 'Illumina FASTA file as contigs/draft genome')
+    parser = argparse.ArgumentParser(prog = 'SASpector - Short-read Assembly inSpector', description = 'Exctract and analyse missing regions from short-read assemblies.')
+    parser.add_argument('reference', type = str, help = 'Reference genome FASTA file, e.g. from hybrid assembly.\n If the file contains multiple seqences, only the first one is used, so make sure to concatenate if needed.')
+    parser.add_argument('contigs', type = str, help = 'Contigs FASTA file from short-read assembly')
     parser.add_argument('-p', '--prefix', type = str, help = 'Genome ID')
     parser.add_argument('-dir', '--outdir', help = 'Output directory')
-    parser.add_argument('-f', '--flanking', nargs = '?' ,const = 'flanking', type = int, help = 'Add flanking regions [Default = 0]', default = 0)
-    parser.add_argument('-k', '--kmers', nargs = '?' ,const = 'kmers', type = int, help = 'Calculate kmer frequencies', default = 0)
+    parser.add_argument('-f', '--flanking', nargs = '?' ,const = 'flanking', type = int, help = 'Add flanking regions [Default = 0 bp]', default = 0)
+    parser.add_argument('-k', '--kmers', nargs = '?' ,const = 'kmers', type = int, help = 'Calculate kmer frequencies (provide k)', default = 0)
     parser.add_argument('-q','--quast', help = 'Run QUAST for unmapped regions against reference assembly', action = 'store_true')
+    parser.add_argument('-c', '--coverage', nargs='?', const='coverage', metavar='bamfile', type = str, help = 'Run SAMtools bedcov to look at short-read coverage in the missing regions. Needs alignment of reads to the reference genome in bam format')
 
     args = parser.parse_args()
     
@@ -33,13 +35,15 @@ def main():
         os.makedirs(args.outdir)
     
     mauve(args.reference, args.contigs, args.prefix, args.outdir)
-    extract_main(args.reference, args.prefix, args.flanking, args.outdir)
+    mappedlocations, unmappedlocations, conflictlocations, reverselocations = extract_main(args.reference, args.prefix, args.flanking, args.outdir)
     prokka(args.prefix, args.outdir)
     blast(args.outdir, args.prefix)
     if args.kmers:
         kmer(args.kmers, args.prefix, args.outdir)
     if args.quast is True:
-         quast(args.reference, args.outdir, args.prefix)
+        quast(args.reference, args.outdir, args.prefix)
+    if args.coverage:
+        cvg_main(mappedlocations, conflictlocations, args.coverage, args.reference, args.outdir, args.prefix)
     
     print('Done!')
 
