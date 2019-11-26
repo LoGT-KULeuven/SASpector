aa6e62303dc3d390baa06f5d98c7078977004b93
emmaver <50531662+emmaver@users.noreply.github.com>
2019-11-26 10:44:45 +0100

    Update coverage.py

--- tool/coverage.py
+++ tool/coverage.py
@@ -9,12 +9,10 @@ import pandas as pd
 import csv
 from Bio import SeqIO
 import subprocess
-import shlex
 import os
 import progressbar
 
 # bwa mem {inputRef} {input.R1} {input.R2} | samtools view -b -o -- | bwa index-a bwtsw {inputREF}
-
 
 
 #make bedfiles for mapped and unmapped regions
@@ -53,17 +51,12 @@ def make_bed(mappedlocations, conflictlocations, reference, outdir, prefix):
             writerunmap.writerow(region)
 
 
-def sam(bamfile, outdir, prefix):
+def sam(bamfile, outdir, prefix, ref):
     sort = 'samtools sort -o {outdir}/coverage/{prefix}.sorted.bam {bam} &&'.format(outdir = outdir, prefix = prefix, bam = bamfile)
     index = 'samtools index {outdir}/coverage/{prefix}.sorted.bam &&'.format(outdir = outdir, prefix = prefix)
     bedcovu = 'samtools bedcov {outdir}/coverage/{prefix}_unmappedregions.bed {outdir}/coverage/{prefix}.sorted.bam > {outdir}/coverage/{prefix}_unmapcvg.tsv &&'.format(outdir = outdir, prefix = prefix)
     bedcovm = 'samtools bedcov {outdir}/coverage/{prefix}_mappedregions.bed {outdir}/coverage/{prefix}.sorted.bam > {outdir}/coverage/{prefix}_mapcvg.tsv'.format(outdir = outdir, prefix = prefix)
-    #cmd = sort+index+bedcovu+bedcovm
-    #cmd = cmd.format(prefix = prefix, outdir = outdir, bam = bamfile)
-    #process = subprocess.Popen(shlex.split(cmd), stdout = subprocess.PIPE)
-    #while process.poll() is None:
-    #    l = process.stdout.readline()
-
+   
     pipe1 = subprocess.Popen(sort, stdout = subprocess.PIPE)
     pipe2 = subprocess.Popen(index, stdin = pipe1.stdout, stdout = subprocess.PIPE)
     pipe3 = subprocess.Popen(bedcovu, stdin = pipe2.stdout, stdout = subprocess.PIPE)
@@ -71,8 +64,6 @@ def sam(bamfile, outdir, prefix):
     output = pipe4.communicate()
     pipe1.stdout.close()
     print(output.decode())
-
-
 
 
 
@@ -85,15 +76,21 @@ def output(prefix, outdir):
     cvgm = pd.read_table(bedcovm, header=None)
     cvgm['perbaseavg'] = cvgm[3]/(cvgm[2]-cvgm[1])
 
+    cvgu['flag']='unmapped'
+    cvgm['flag']='mapped'
+    cvg=cvgu.append(cvgm)
+    cvg.columns=['seq','start', 'end', 'total_perbase_depth', 'avg_perbase_depth', 'flag']
+    cvg.sort_values(by=['start'])
+    
     #barplot to compare the perbaseavg
 
 
-def cvg_main(mappedlocations, unmappedlocations, conflictlocations, reverselocations, bamfile, reference, outdir, prefix):
+def cvg_main(mappedlocations, conflictlocations, bamfile, reference, outdir, prefix):
     newpath = 'coverage'
     os.makedirs(os.path.join(outdir,newpath))
 
     bar = progressbar.ProgressBar(widgets = ['Running SAMtools: ', progressbar.Bar(), '(', progressbar.ETA(),')'])
     for i in bar(range(1)):
-        sam(bamfile, outdir, prefix)
+        sam(bamfile, outdir, prefix, ref)
         make_bed(mappedlocations, conflictlocations, reference, outdir, prefix)
         output(prefix, outdir)
