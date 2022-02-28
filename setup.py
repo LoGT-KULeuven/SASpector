4d214f5e01c73a8ecf4a1129063ecea0d46b361b
0mician <cedric.lood@kuleuven.be>
2022-02-28 22:53:04 +0100

    sorting out options

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name = 'SASpector',
-    version = '0.0.4',
+    version = '0.0.5',
     author = 'KU Leuven',
     author_email = 'alecorrojo@gmail.com, cedric.lood@kuleuven.be',
     maintainer = 'Computational systems biology group',
